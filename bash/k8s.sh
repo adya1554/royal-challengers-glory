7680b19b23604ef56e97986d6ecffbfda9151d5f
Aditya magadum <adityamagadum108@gmail.com>
2025-12-26 10:16:29 +0530

    Change directory and upgrade Helm chart

--- bash/k8s.sh
+++ bash/k8s.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+cd /home/ubuntu/bashscripts/Day8/react-helmcharts/
+helm upgrade react-helmcharts .
 pods_id=$(kubectl get pods -n react-microk8s |grep react |awk {'print $1'})
 kubectl delete pod $pods_id -n react-microk8s
