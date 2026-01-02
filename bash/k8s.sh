b217c47427383b03b6c373bffc912a6d7f6ef996
aditya <aditya@gmail.com>
2026-01-02 16:23:46 +0530

    miner changes

--- bash/k8s.sh
+++ bash/k8s.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-cd /home/ubuntu/workdir/royal-challengers-glory/react-helm/ # change directory to heml 
+cd /home/ubuntu/workdir/royal-challengers-glory/react-chart/ # change directory to heml 
 
-aws s3 cp s3://git-commit-bucket/old_value.txt . # copy files from s3 to working directory
-aws s3 cp s3://git-commit-bucket/new_value.txt .
+sudo aws s3 cp s3://git-commit-bucket/old_value.txt . # copy files from s3 to working directory
+sudo aws s3 cp s3://git-commit-bucket/new_value.txt .
 
 
 old_value=$(cat old_value.txt) # assingning values to vareable
 new_value=$(cat new_value.txt)
 
 #using sed command cahnge the tag with this tag helm upgrate the version
-sed -i "s/${old_value}/${new_value}/g" values.yaml
+sudo sed -i "s/${old_value}/${new_value}/g" values.yaml
 
 #cahnges will add to staging area and commit with those laatest values
-git add .
-git commit -m "${new_value} is updated"
-git push https://$GIT_TOKEN@github.com/adya1554/royal-challengers-glory.git
+sudo git add .
+sudo git commit -m "${new_value} is updated"
+sudo git push https://$GIT_TOKEN@github.com/adya1554/royal-challengers-glory.git
 
 # git token must be update in server using following commands
 # echo# 'export GIT_TOKEN=Your_generated_token' >> ~/.bashrc
@@ -22,7 +22,7 @@ git push https://$GIT_TOKEN@github.com/adya1554/royal-challengers-glory.git
 
 
 #re,ovce old value from s3 buclet
-aws s3 rm s3://git-commit-bucket/old_value.txt
+sudo aws s3 rm s3://git-commit-bucket/old_value.txt
 
 
 echo $new_value > old_value.txt
